5002f3215e857c65d19e476e6e01df8cf976a599
samyyc <s1myyc@outlook.com>
2025-10-29 00:04:57 +0800

    feat: license

--- AudioApi/IAudioChannel.cs
+++ AudioApi/IAudioChannel.cs
@@ -1,3 +1,21 @@
+/*
+ * Audio - A swiftlys2 plugin to control counter-strike 2 in-game VoIP audio stream.
+ * Copyright (C) 2025  samyyc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace AudioApi;
 
 /// <summary>
